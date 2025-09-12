75edde587f1571026ead0236c09706b8072e1284
nina <shuanshuankao1222@example.com>
2025-09-12 20:34:34 +0800

    修改使用ModelForm版本

--- todo/views.py
+++ todo/views.py
@@ -1,30 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import json
 from .models import Todo
+from .forms import TodoForm
 
 
 def create_todo(request):
     message = ""
-
+    form = TodoForm()
     # POST
     if request.method == "POST":
-        print(request.POST)
-        title = request.POST.get("title")
-        if title == "":
-            print("標題欄位不能空!")
-            message = "標題欄位不能空!"
-        else:
-            text = request.POST.get("text")
-            important = request.POST.get("important")
+        form = TodoForm(request.POST)
+        form.save()
+        message = "建立成功 !"
+        return redirect("todolist")
 
-            important = True if important == "on" else False
-
-            # 建立資料
-            todo = Todo.objects.create(title=title, text=text, important=important)
-            todo.save()
-            message = "建立成功 !"
-    return render(request, "todo/create-todo.html", {"message": message})
+    return render(request, "todo/create-todo.html", {"message": message, "form": form})
 
 
 def view_todo(request, id):
@@ -42,7 +33,8 @@ def view_todo(request, id):
 
 
 def todolist(request):
-    todos = Todo.objects.all()
+    # order_by("-created") => 降序
+    todos = Todo.objects.all().order_by("-created")
 
     return render(request, "todo/todolist.html", {"todos": todos})
 
