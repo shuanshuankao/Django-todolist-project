36a2990420f9ae76b852f5b0733547dd8d217df3
nina <shuanshuankao1222@example.com>
2025-09-12 19:45:18 +0800

    新增代辦事項完成

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,29 @@ import json
 from .models import Todo
 
 
+def create_todo(request):
+    message = ""
+
+    # POST
+    if request.method == "POST":
+        print(request.POST)
+        title = request.POST.get("title")
+        if title == "":
+            print("標題欄位不能空!")
+            message = "標題欄位不能空!"
+        else:
+            text = request.POST.get("text")
+            important = request.POST.get("important")
+
+            important = True if important == "on" else False
+
+            # 建立資料
+            todo = Todo.objects.create(title=title, text=text, important=important)
+            todo.save()
+            message = "建立成功 !"
+    return render(request, "todo/create-todo.html", {"message": message})
+
+
 def view_todo(request, id):
     todo = None
     try:
