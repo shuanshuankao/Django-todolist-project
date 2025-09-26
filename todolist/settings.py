ff09679bf9758665188951dd5b9719d2e54ae1e5
nina <shuanshuankao1222@example.com>
2025-09-26 21:16:58 +0800

    0926更新

--- todolist/settings.py
+++ todolist/settings.py
@@ -98,7 +98,7 @@ else:
             "USER": os.environ.get("USER"),
             "PASSWORD": os.environ.get("PASSWORD"),
             "HOST": os.environ.get("HOST"),
-            "PORT": os.environ.get("PORT"),
+            "PORT": int(os.environ.get("PORT")),
         }
     }
 
